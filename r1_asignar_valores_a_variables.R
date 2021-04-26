655a1cf3fdf2253fe85ed1997846b7817151b3db
GandyA23 <avila.gandy@gmail.com>
2021-04-26 12:30:09 -0500

    Se cambió el nombre de los archivos para que sea posible ver un orden

---
+++ r1_asignar_valores_a_variables.R
@@ -0,0 +1,22 @@
+# Gandy Esau Avila Estrada
+# Asignación de valores
+
+# <- operador de asignación 
+
+# A la variable a se le asigna el valor de 5
+a<-5
+
+# Imprime el valor de a en la consola
+a
+
+# A la variable b se le asigna el valor de 8
+b<-8  
+
+# Imprime el valor de b en la consola
+b
+
+# A la variable c se le asigna la secuencia del 1 al 20
+c<-1:20
+
+# Imprime el valor de la variable c en consola
+c
