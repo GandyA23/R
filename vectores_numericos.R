29b659488b4742b9e60687202e86d69e49380378
GandyA23 <avila.gandy@gmail.com>
2021-04-26 11:59:20 -0500

    Script: Vectores numéricos

---
+++ vectores_numericos.R
@@ -0,0 +1,23 @@
+# Gandy Esau Avila Estrada
+# Vectores numéricos
+
+# Se crea el vector x con la función c (concatenar)
+x <- c(1, 2, 3)
+
+x
+
+# Se crea el vector y con la función c (concatenar)
+y <- c(4, 5, 6)
+
+y
+
+# Imprime la concatenación de los vectores x, y
+c(x, y)
+
+# Otra manera de asignar valores a variables
+assign("z", c(7, 8, 9))
+
+z
+
+# Imprime la concatenación de los vectores x, y, z
+c(x, y, z)
