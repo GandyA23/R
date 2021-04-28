604d5e7b20baba2e041e136cc2b5ed8e27b11a83
GandyA23 <avila.gandy@gmail.com>
2021-04-28 12:08:54 -0500

    Script: Funciones any() y all()

---
+++ r6_any_all.R
@@ -0,0 +1,22 @@
+# Gandy Esau Avila Estrada
+# Funciones all() y any()
+
+# Ambas funciones retornan un valor booleano. 
+
+# Tenemos un vector con una secuencia de números del 1 al 20
+x <- seq(20)
+
+# Si cualquier valor en el vector es mayor a 50
+any (x > 50)
+
+# Si cualquier valor en el vector es mayor a 5
+any (x > 5)
+
+# Tenemos un vector con una secuencia de números del 1 al 5
+y <- seq(5)
+
+# Si todos los valores son mayores a 1
+all (y > 1)
+
+# Si todos los valores son mayores a 0
+all (y > 0)   
